a691cfe41670ca60964df1839a9844dc4a8a82f6
Worasit D <worasit.dmk501@gmail.com>
2020-09-09 10:16:18 +0700

    Initilized a project with basic RESTful API application structure.

---
+++ src/test/java/org/worasit/ams/server/ServerApplicationTests.java
@@ -0,0 +1,11 @@
+package org.worasit.ams.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
