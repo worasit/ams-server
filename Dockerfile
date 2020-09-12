8d1badbdddc75d2cc4f72b8b9bb8698087cd2840
Worasit D <worasit.dmk501@gmail.com>
2020-09-12 19:10:02 +0700

    updated Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
-ARG JAR_FILE
-COPY ${JAR_FILE} /opt/ams-server/app.jar
+COPY target/*.jar /opt/ams-server/app.jar
 ENTRYPOINT ["java","-jar","/opt/ams-server/app.jar"]
--- scripts/docker/push-image.sh
+++ scripts/docker/push-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 docker login -u ${DOCKER_USER_ENV} -p ${DOCKER_PASS_ENV}
-docker build --build-arg JAR_FILE="target/*.jar" -t ${DOCKER_USER_ENV}/ams-server .
+docker build -t ${DOCKER_USER_ENV}/ams-server .
 docker push ${DOCKER_USER_ENV}/ams-server:latest
