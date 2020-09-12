96695cb941375c3972bcef462c92b953b2afe6e8
Worasit D <worasit.dmk501@gmail.com>
2020-09-12 14:43:52 +0700

    [AMS-SERVER-1] Applied OpenAPI3.0 document.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AMS
 The prototype of apartment management system
+
+# Technology Stack
+- RESTful: https://spring.io/guides/tutorials/rest/
+- OpenApi : https://www.baeldung.com/spring-rest-openapi-documentation
--- src/main/java/org/worasit/ams/server/controller/GreetingController.java
+++ src/main/java/org/worasit/ams/server/controller/GreetingController.java
@@ -1,8 +1,12 @@
 package org.worasit.ams.server.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.worasit.ams.server.model.Greeting;
@@ -12,11 +16,24 @@ public class GreetingController {
   String template = "Hello, %s!";
   AtomicLong counter = new AtomicLong();
 
-  @RequestMapping(method = RequestMethod.GET, path = "/greeting")
-  public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+  @Operation(summary = "Say greeting to a guest")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Say greeting to specific guest",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = Greeting.class))
+            })
+      })
+  @GetMapping(path = "/greeting")
+  public Greeting greeting(
+      @RequestParam(defaultValue = "World", name = "Guest's name") String guestName) {
     return Greeting.builder()
         .id(counter.incrementAndGet())
-        .content(String.format(template, name))
+        .content(String.format(template, guestName))
         .build();
   }
 }
