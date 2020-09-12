6de2102cef723d170dfb9294fd39b173f74db946
Worasit D <worasit.dmk501@gmail.com>
2020-09-12 19:44:18 +0700

    updated Dockerfile.

--- scripts/docker/push-image.sh
+++ scripts/docker/push-image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+mvn package -Dmaven.test.skip=true
 docker login -u ${DOCKER_USER_ENV} -p ${DOCKER_PASS_ENV}
 docker build -t ${DOCKER_USER_ENV}/ams-server .
 docker push ${DOCKER_USER_ENV}/ams-server:latest
